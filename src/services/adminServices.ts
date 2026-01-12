0cc2b970747174dfa37745278c888ad994b5dbde
Vincent <vincenttayros078@gmail.com>
2026-01-12 15:54:39 +0800

    added Adminservices

---
+++ src/services/adminServices.ts
@@ -0,0 +1,75 @@
+const API_URL = 'http://localhost:3001/api/admin';
+
+export class AdminServices{
+
+            static async getAllUsers(){
+                  try{
+                      
+                    const users = await fetch(`${API_URL}/users`,{
+                        credentials:'include'
+                    }); 
+
+                    const result = await users.json();
+                        
+                    if(!result.success){
+                       return{
+                        success:false,
+                        message:result.message
+                       }
+                    }    
+
+                    return{
+                        success:true,
+                        message:result.message
+                    }
+
+
+                  }catch(e){
+                      console.error(e);
+                      return{
+                        success:false,
+                        message:"Failed to fetch users"
+                      }
+                  }
+
+
+            }
+              
+            static async getDelete(id:number){
+                
+                try{
+                   
+                    const deleteResponse = await fetch(`${API_URL}/users/${id}`,{
+                        method:"DELETE",
+                        credentials:'include'
+                    });
+
+                    const deleteResult = await deleteResponse.json();
+
+                    if(!deleteResult.success){
+                        return{
+                            sucess:false,
+                            message:deleteResult.message
+                        }
+                    }
+
+                    return{
+                        success:true,
+                        message:deleteResult.message
+                    }
+
+
+                }catch(e){
+                    console.error(e);
+                    return{
+                        success:false,
+                        message:"Failed to fetch deletee"
+                    }
+                }
+
+
+            }
+
+
+
+}
